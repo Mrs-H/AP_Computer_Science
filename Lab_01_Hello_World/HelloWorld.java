196a9a904b32a03dd75077c4bab3bb40771e20c3
Mrs. H <eheinricher@aapl000858-2.local>
2016-08-15 19:05:57 -0400

    HelloWorld.java first program

---
+++ Lab_01_Hello_World/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * HelloWorld.java a classic first Java program
+ * 
+ * @author Mrs. Heinricher
+ * @version 01
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
